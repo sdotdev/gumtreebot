7986a209691dddfeb6e7cc548d071636f2b00740
indesisive <83420002+sdotdev@users.noreply.github.com>
2026-05-13 00:08:14 +0100

    x

--- scraper/lib/supabase.js
+++ scraper/lib/supabase.js
@@ -1,10 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
+import WebSocket from 'ws'
 
 export const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY,
-  { 
+  {
     auth: { persistSession: false },
-    realtime: { enabled: false }
+    realtime: { transport: WebSocket }
   }
 )
