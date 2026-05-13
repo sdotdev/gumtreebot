938b0901e2c570849765e860e31a4820ae1881e1
indesisive <83420002+sdotdev@users.noreply.github.com>
2026-05-13 01:04:20 +0100

    Update layout.tsx

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -20,15 +20,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
         <Toaster />
       </body>
